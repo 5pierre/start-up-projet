3a4025b9fccfb80d3390506aafc8b6c8b04b6454
5pierre <pllieske@outlook.fr>
2026-01-20 00:38:16 +0100

    update

--- frontend/src/components/RegisterPage.jsx
+++ frontend/src/components/RegisterPage.jsx
@@ -102,7 +102,7 @@ export default function RegisterPage(){
 
     setIsSubmitting(true);
 
-    const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost';
+    const BASE_URL = process.env.REACT_APP_API_AUTH_URL;
     const endpoint = isLoginMode ? 'login' : 'register';
     const url = `${BASE_URL}/api/auth/${endpoint}`;
 
